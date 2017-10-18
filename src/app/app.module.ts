e8020de6088fd513aa5b21403972ef15f36d19ec
Sudheer Kumar <s.k.r1thor@gmail.com>
2017-10-18 22:52:38 +0530

    update appmodule file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+// imports for loading & configuring the in-memory web-api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { HeroService } from '../services/hero.service';
 import { AppComponent } from './app.component';
@@ -21,6 +26,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [HeroService],
